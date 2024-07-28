ba27a6e376cca56a72f571ab9f1fd4179da2b9d4
Kawin Glomjai <g.kawin@live.com>
2024-07-28 10:17:40 -0700

    new game logic

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,13 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.*;
+
 /**
  * TicTacToeSwing
  */
@@ -9,48 +19,48 @@ public class TicTacToe {
     }
 }
 
-class TicTacToePanel extends javax.swing.JFrame {
+class TicTacToePanel extends JFrame {
 
     public TicTacToePanel() {
         initComponents();
     }
 
     private void initComponents() {
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Tic Tac Toe");
         setSize(400, 400);
-        setLayout(new java.awt.GridLayout(4, 4));
+        setLayout(new GridLayout(4, 4));
 
         for (int i = 0; i < 9; i++) {
-            javax.swing.JButton button = new javax.swing.JButton();
-            button.setFont(new java.awt.Font("Arial", 1, 48));
-            button.addActionListener(new java.awt.event.ActionListener() {
-                public void actionPerformed(java.awt.event.ActionEvent evt) {
+            JButton button = new JButton();
+            button.setFont(new Font("Arial", 1, 48));
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent evt) {
                     buttonActionPerformed(evt);
                 }
             });
             add(button);
         }
 
-        javax.swing.JLabel label = new javax.swing.JLabel();
-        label.setFont(new java.awt.Font("Arial", 4, 16));
-        label.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        JLabel label = new JLabel();
+        label.setFont(new Font("Arial", 4, 16));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setText("Player " + 1 + "'s Turn");
         add(label);
 
-        javax.swing.JButton newGamebtn = new javax.swing.JButton();
-        newGamebtn.setFont(new java.awt.Font("Arial", 1, 16));
+        JButton newGamebtn = new JButton();
+        newGamebtn.setFont(new Font("Arial", 1, 16));
         newGamebtn.setText("New Game");
-        newGamebtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-
+        newGamebtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                newGameActionPerformed(evt);
             }
         });
         add(newGamebtn);
     }
 
-    private void buttonActionPerformed(java.awt.event.ActionEvent evt) {
-        javax.swing.JButton button = (javax.swing.JButton) evt.getSource();
+    private void buttonActionPerformed(ActionEvent evt) {
+        JButton button = (JButton) evt.getSource();
         if (button.getText().equals("")) {
             button.setText("1");
         } else if (button.getText().equals("1")) {
@@ -60,8 +70,11 @@ class TicTacToePanel extends javax.swing.JFrame {
         }
     }
 
-    private void newGameActionPerformed(java.awt.event.ActionEvent evt, javax.swing.JButton button) {
-        // Clear all buttons.
+    private void newGameActionPerformed(java.awt.event.ActionEvent evt) {
+        for (int i = 0; i < 9; i++) {
+            JButton button = (JButton) getContentPane().getComponent(i);
+            button.setText("");
+        }
     }
 
 }
